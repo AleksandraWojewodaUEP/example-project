c4af33dc52e4001f12e525f177a81c1b87869961
Aleksandra Wojewoda <aleksandra.wojewoda@ue.poznan.pl>
2023-04-16 17:21:28 +0200

    Dodany trojkat

---
+++ exercise_zaoczni/ex_2.py
@@ -0,0 +1,38 @@
+# trojkat
+
+a = 40
+b = 12
+c = 8
+h = 6
+pole = 1/2 * a * h
+obwod = a + b + c
+
+print("Pole trojkata wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# trojkat
+
+a = 10
+b = 20
+c = 15
+h = 12
+
+obwod = a + b + c
+pole = int((h * a) / 2)
+
+# print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
